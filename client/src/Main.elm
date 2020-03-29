08ab5e8fcde4d529b4acd8591c28a0d5c55ec1e1
mdarocha <git@mdarocha.pl>
2020-03-29 15:37:48 +0200

    Load data from api

--- client/src/Main.elm
+++ client/src/Main.elm
@@ -4,8 +4,9 @@ import Browser
 import Browser.Navigation as Nav
 import Html exposing (..)
 import Url
+import Http
 
-main : Program () Model Msg
+main : Program String Model Msg
 main =
     Browser.application
         { init = init
@@ -17,32 +18,55 @@ main =
         }
 
 -- MODEL
-type alias Model =
-    { text : String
-    }
+type Model
+    = Failure
+    | Loading
+    | Success String
 
-init : () -> Url.Url -> Nav.Key -> ( Model, Cmd Msg)
-init flags url key =
-    (Model "Hello world", Cmd.none)
+init : String -> Url.Url -> Nav.Key -> ( Model, Cmd Msg)
+init api url key =
+    ( Loading
+    , Http.get
+        { url = api ++ "trainstop/get"
+        , expect = Http.expectString GotStops
+        }
+    )
 
 
 -- UPDATE
 type Msg
     = LinkClicked Browser.UrlRequest
     | UrlChanged Url.Url
+    | GotStops (Result Http.Error String)
 
 update : Msg -> Model -> ( Model, Cmd Msg )
-update _ model =
-    ( model, Cmd.none )
+update msg model =
+    case msg of
+        GotStops result ->
+            case result of
+                Ok text ->
+                    (Success text, Cmd.none)
+                Err _ ->
+                    (Failure, Cmd.none)
+        _ ->
+            ( model, Cmd.none )
 
 -- SUBSCRIPTIONS
 subscriptions : Model -> Sub Msg
 subscriptions _ =
     Sub.none
 
+-- VIEw
 view : Model -> Browser.Document Msg
 view model =
     { title = "Trains"
     , body =
-        [ text model.text ]
+        [ case model of
+            Failure ->
+                text "Error"
+            Loading ->
+                text "Loading"
+            Success result ->
+                text result
+        ]
     }
