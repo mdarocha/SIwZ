69a711ba12486d2177c52878d1d717549cf8dbf7
mdarocha <git@mdarocha.pl>
2020-03-29 16:00:32 +0200

    Display list of elements

--- client/src/Main.elm
+++ client/src/Main.elm
@@ -5,6 +5,7 @@ import Browser.Navigation as Nav
 import Html exposing (..)
 import Url
 import Http
+import Json.Decode as Json
 
 main : Program String Model Msg
 main =
@@ -18,36 +19,36 @@ main =
         }
 
 -- MODEL
-type Model
+type Stops
     = Failure
     | Loading
-    | Success String
+    | Success (List TrainStop)
+
+type alias Model =
+    { api : String
+    , stops: Stops
+    }
 
 init : String -> Url.Url -> Nav.Key -> ( Model, Cmd Msg)
 init api url key =
-    ( Loading
-    , Http.get
-        { url = api ++ "trainstop/get"
-        , expect = Http.expectString GotStops
-        }
-    )
+    ( Model api Loading, getStops api )
 
 
 -- UPDATE
 type Msg
     = LinkClicked Browser.UrlRequest
     | UrlChanged Url.Url
-    | GotStops (Result Http.Error String)
+    | GotStops (Result Http.Error (List TrainStop))
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         GotStops result ->
             case result of
-                Ok text ->
-                    (Success text, Cmd.none)
+                Ok stops ->
+                    ( { model | stops = Success stops }, Cmd.none)
                 Err _ ->
-                    (Failure, Cmd.none)
+                    ( { model | stops = Failure }, Cmd.none)
         _ ->
             ( model, Cmd.none )
 
@@ -56,17 +57,43 @@ subscriptions : Model -> Sub Msg
 subscriptions _ =
     Sub.none
 
--- VIEw
+-- VIEW
 view : Model -> Browser.Document Msg
 view model =
     { title = "Trains"
     , body =
-        [ case model of
+        [ case model.stops of
             Failure ->
                 text "Error"
             Loading ->
                 text "Loading"
-            Success result ->
-                text result
+            Success stops ->
+                ul []
+                    (List.map (\s -> li [] [ text s.name ]) stops)
         ]
     }
+
+-- HTTP
+getStops : String -> Cmd Msg
+getStops api =
+    Http.get
+        { url = api ++ "trainstop/get"
+        , expect = Http.expectJson GotStops stopsDecoder
+        }
+
+type alias TrainStop =
+    { id: String
+    , city: String
+    , name: String
+    }
+
+stopsDecoder : Json.Decoder (List TrainStop)
+stopsDecoder =
+    Json.list stopDecoder
+
+stopDecoder : Json.Decoder TrainStop
+stopDecoder =
+    Json.map3 TrainStop
+        (Json.field "id" Json.string)
+        (Json.field "city" Json.string)
+        (Json.field "name" Json.string)
