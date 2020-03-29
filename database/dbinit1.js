3c48184b23d7fa19fd48947011bd506c9dfd9a3b
zygmuntdzwigon <zygmuntdxwigon@gmail.com>
2020-03-29 17:40:56 +0200

    db seed

---
+++ database/dbinit1.js
@@ -0,0 +1,24 @@
+db = db.getSiblingDB('TrainSystem');
+
+db.createCollection("TrainStops");
+
+db.TrainStops.insertMany([
+    {
+        name: "Main train station",
+        city: "Cracov"
+    },
+    {
+        name: "Plaszow",
+        city: "Cracov"
+    },
+    {
+        name: "Main train station",
+        city: "Warsaw"
+    },
+    {
+        name: "West train station",
+        city: "Rzeszow"
+    }]
+);
+
+db.TrainStops.save();
