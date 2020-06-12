b73aa6d92856bf2a12197e9ba00d8baa680437b6
zygmuntdzwigon <zygmuntdxwigon@gmail.com>
2020-06-12 19:51:24 +0200

    ticketDTO update

--- server/ModelsDTO/TicketDTO.cs
+++ server/ModelsDTO/TicketDTO.cs
@@ -1,25 +1,26 @@
 using System;
+using Server.Models;
 
 namespace Server.ModelsDTO
 {
     public class TicketDTO
     {
         public int Id { set; get; }
-        
-        public int Price { set; get; }
-        
-        public String Discount { set; get; }
-        
-        public string From { set; get; } 
 
-        public string To { set; get; }
-        
+        public int Price { set; get; }
+
+        public String Discount { set; get; }
+
+        public TrainStop From { set; get; }
+
+        public TrainStop To { set; get; }
+
         public string TrainName { set; get; }
 
         public int WagonNo { set; get; }
 
         public int SeatNo { set; get; }
-        
+
         public DateTime RideDate { set; get; }
     }
 }
